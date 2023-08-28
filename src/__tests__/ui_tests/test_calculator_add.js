73387964695d666c5737a6ce9bf3c372e2b78a9b
Anthony <abtrini@gmail.com>
2023-08-28 10:42:09 -0400

    Commit

--- src/__tests__/ui_tests/test_calculator_add.js
+++ src/__tests__/ui_tests/test_calculator_add.js
@@ -1,36 +1,36 @@
-const { Builder, By, Key, until } = require("selenium-webdriver");
-const assert = require("assert");
+// const { Builder, By, Key, until } = require("selenium-webdriver");
+// const assert = require("assert");
 
-//jest.setTimeout(10000);
+// jest.setTimeout(10000);
 
-describe("Calculator Integration Tests", () => {
-  test("performs subtraction correctly", async () => {
-    const driver = await new Builder().forBrowser("chrome").build();
-    try {
-      await driver.get("https://learn-cicd-anthony-24f4f0f46557.herokuapp.com/"); // Replace with the actual URL of your React app
+// describe("Calculator Integration Tests", () => {
+//   test("performs subtraction correctly", async () => {
+//     const driver = await new Builder().forBrowser("chrome").build();
+//     try {
+//       await driver.get("https://learn-cicd-anthony-24f4f0f46557.herokuapp.com/"); // Replace with the actual URL of your React app
 
-      await driver
-      .findElement(By.id("Number 1"))
-      .sendKeys("10");
-    await driver.findElement(By.css("select")).sendKeys("+");
-    await driver
-      .findElement(By.id("Number 2"))
-      .sendKeys("3");
-    await driver.findElement(By.xpath('//button[text()="Calculate"]')).click();
+//       await driver
+//       .findElement(By.id("Number 1"))
+//       .sendKeys("10");
+//     await driver.findElement(By.css("select")).sendKeys("+");
+//     await driver
+//       .findElement(By.id("Number 2"))
+//       .sendKeys("3");
+//     await driver.findElement(By.xpath('//button[text()="Calculate"]')).click();
 
-    await driver.wait(
-      until.elementLocated(By.xpath('//*[@id="root"]/div/h2')),
-      5000
-    );
-    const resultElement = await driver.findElement(
-      By.xpath('//*[@id="root"]/div/h2')
-    );
-    const resultText = await resultElement.getText();
-    assert.strictEqual(resultText, "Result: 13");function add(a, b) {
-  return a + b;
-}
-    } finally {
-     // await driver.quit();
-    }
-  });
-});
+//     await driver.wait(
+//       until.elementLocated(By.xpath('//*[@id="root"]/div/h2')),
+//       5000
+//     );
+//     const resultElement = await driver.findElement(
+//       By.xpath('//*[@id="root"]/div/h2')
+//     );
+//     const resultText = await resultElement.getText();
+//     assert.strictEqual(resultText, "Result: 13");function add(a, b) {
+//   return a + b;
+// }
+//     } finally {
+//      await driver.quit();
+//     }
+//   });
+// });
